2c46e158e9854c3068b95dd6c737a8c1a1686eae
jh83 <16517831+jh83@users.noreply.github.com>
2023-11-13 08:38:13 +0100

    Add files via upload

---
+++ Azure_IoT_Hub_ESP8266/iot_configs.h
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// SPDX-License-Identifier: MIT
+
+// Wifi
+#define IOT_CONFIG_WIFI_SSID "xx"
+#define IOT_CONFIG_WIFI_PASSWORD "xx"
+
+// Azure IoT
+#define IOT_CONFIG_IOTHUB_FQDN "xx"
+#define IOT_CONFIG_DEVICE_ID "xx"
+#define IOT_CONFIG_DEVICE_KEY "xx"
+
+// Publish 1 message every 2 seconds
+#define TELEMETRY_FREQUENCY_MILLISECS 2000
